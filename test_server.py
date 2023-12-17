9ccd8f2c92e7a41d9e882610cf1ea5450612a41e
Carlos Heredero <herederocarlos9@gmail.com>
2023-12-17 16:20:15 +0800

    Initial commit

---
+++ test_server.py
@@ -0,0 +1,35 @@
+import sys
+from python_banyan.banyan_base import BanyanBase
+
+
+class EchoServer(BanyanBase):
+    """
+    This class is a simple Banyan echo server
+    """
+
+    def __init__(self, ):
+
+        # initialize the parent
+        super(EchoServer, self).__init__(process_name='EchoServer')
+
+        # subscribe to receive 'echo' messages from the client
+        self.set_subscriber_topic('echo')
+
+        # wait for messages to arrive
+        try:
+            self.receive_loop()
+        except KeyboardInterrupt:
+            self.clean_up()
+            sys.exit(0)
+
+    def incoming_message_processing(self, topic, payload):
+        """
+        Process incoming messages from the client
+        :param topic: message topic
+        :param payload: message payload
+        """
+        # republish the message with a topic of reply
+        self.publish_payload(payload, 'reply')
+
+        # extract the message number from the payload
+        print('Message number:', payload['message_number'])
