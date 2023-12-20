658649f2140071ff319d2bf3943a4fe56deebeb4
DarsVelasco <darwin.velasco1210@gmail.com>
2023-12-20 21:53:47 +0800

    tester_client_Update_TIME_DISABLE

--- tester_client_tk.py
+++ tester_client_tk.py
@@ -1,74 +1,70 @@
 from __future__ import unicode_literals
 from tkinter import messagebox
 import time
-
+import sys
 
 try:
     from tkinter import *
-    from tkinter import font
     from tkinter import ttk
     from tkinter import simpledialog
 except ImportError:
     from Tkinter import *
-    import tkFont as font
     import ttk
-
 
 import msgpack
 import zmq
 from python_banyan.banyan_base import BanyanBase
 
-class TkEchoClient(BanyanBase):
 
-    def __init__(self,topics='reply',back_plane_ip_address=None, subscriber_port='43125',
+class CountdownWindow(Toplevel):
+    def __init__(self, parent, duration, callback):
+        super().__init__(parent)
+        self.title("Set Countdown Timer")
+        self.geometry("300x150")
+
+        self.duration_label = ttk.Label(self, text="Set countdown duration (seconds):")
+        self.duration_label.pack(pady=10)
+
+        self.duration_entry = ttk.Entry(self)
+        self.duration_entry.pack(pady=10)
+
+        self.set_button = ttk.Button(self, text="Set Timer", command=self.start_countdown)
+        self.set_button.pack(pady=10)
+
+        self.duration = duration
+        self.callback = callback  # Fixed: Make sure to set the callback
+
+    def start_countdown(self):
+        try:
+            self.duration = int(self.duration_entry.get())
+            self.callback(self.duration)  # Fixed: Call the callback with the duration
+            self.destroy()
+        except ValueError:
+            messagebox.showerror("Error", "Invalid input. Please enter a valid integer.")
+
+
+class TkEchoClient(BanyanBase):
+    def __init__(self, topics='reply', back_plane_ip_address=None, subscriber_port='43125',
                  publisher_port='43124', process_name='Auction IO'):
-        # Prompt the user for their name
         self.user_name = simpledialog.askstring("Your Name", "Enter your name:")
         if not self.user_name:
-            sys.exit(0)  # Exit if the user cancels the input
+            sys.exit(0)
 
-
-        # Call the constructor of BanyanBase to initialize the publisher
         super(TkEchoClient, self).__init__(back_plane_ip_address=back_plane_ip_address,
                                            subscriber_port=subscriber_port,
                                            publisher_port=publisher_port,
                                            process_name=process_name)
-        
-        # Set the subscriber topics
+
         if topics is None:
             raise ValueError('No Topic List Was Specified.')
 
         for x in topics:
             self.set_subscriber_topic(x)
 
-        # Display a message on the server that the client is ready for auction
         ready_message = f"{self.user_name} is ready for auction"
         payload = {'Action': 'ReadyForAuction', 'Message': ready_message, 'Seller': self.user_name}
         self.publish_payload(payload, 'echo')
 
-        # ... (your existing __init__ code)
-
-        # ... (your existing GUI setup code)
-        # establish some banyan variables
-        self.back_plane_ip_address = back_plane_ip_address
-        self.subscriber_port = subscriber_port
-        self.publisher_port = publisher_port
-
-        # subscribe to the topic
-        if topics is None:
-            raise ValueError('No Topic List Was Specified.')
-
-        # initialize the banyan base class
-        super(TkEchoClient, self).__init__(back_plane_ip_address=back_plane_ip_address,
-                                           subscriber_port=subscriber_port,
-                                           publisher_port=publisher_port,
-                                           process_name=process_name)
-
-        # subscribe to all topics specified
-        for x in topics:
-            self.set_subscriber_topic(x)
-
-        # setup root window
         self.root = Tk()
         self.root.title(f"Auction.IO | {self.user_name}")
 
@@ -87,14 +83,13 @@ class TkEchoClient(BanyanBase):
         self.items_selling = Listbox(self.display_sell_frame, width=50, height=10)
         self.items_selling.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 
-        self.sell_button = ttk.Button(self.display_sell_frame, text="Sell", command=None, state="normal")
+        self.sell_button = ttk.Button(self.display_sell_frame, text="Sell", command=self.place_sell, state="normal")
         self.sell_button.grid(row=1, column=0, padx=(5, 0), pady=10, sticky="w")
 
-        self.delete_sell_button = ttk.Button(self.display_sell_frame, text="Delete", command=None,
+        self.delete_sell_button = ttk.Button(self.display_sell_frame, text="Delete", command=self.delete_sell_item,
                                              state="normal")
         self.delete_sell_button.grid(row=1, column=1, padx=(0, 5), pady=10, sticky="e")
 
-        # Adjust column weights
         self.display_sell_frame.columnconfigure(0, weight=1)
         self.display_sell_frame.columnconfigure(1, weight=1)
 
@@ -104,7 +99,7 @@ class TkEchoClient(BanyanBase):
         self.items_bidding = Listbox(self.display_bid_frame, width=50, height=10)
         self.items_bidding.grid(row=0, column=0, padx=10, pady=10)
 
-        self.bid_button_bidding = ttk.Button(self.display_bid_frame, text="Bid", command=None, state="normal")
+        self.bid_button_bidding = ttk.Button(self.display_bid_frame, text="Bid", command=self.place_bid, state="normal")
         self.bid_button_bidding.grid(row=1, column=0, padx=10, pady=10)
 
         self.highest_bidder_frame = ttk.LabelFrame(self.frame, text="Highest Bidder")
@@ -113,16 +108,47 @@ class TkEchoClient(BanyanBase):
         self.highest_bidder_list = Listbox(self.highest_bidder_frame, width=110, height=10)
         self.highest_bidder_list.grid(row=0, column=0, padx=10, pady=10, sticky="nsew", columnspan=2)
 
+        self.timer_seconds = 0  # Initialize timer to 0
+        self.timer_active = False
+
+        self.timer_popup_button = ttk.Button(self.countdown_frame, text="Set Timer", command=self.set_timer)
+        self.timer_popup_button.grid(row=0, column=1, padx=10, pady=10)
         self.sell_button.config(command=self.place_sell)
         self.delete_sell_button.config(command=self.delete_sell_item)
         self.bid_button_bidding.config(command=self.place_bid)
 
         self.highest_bids = {}
         self.bidders_names = {}
+        self.disable_duration = 0  # Add this line to initialize disable_duration
+        
 
-        # self.root.after(2, self.place_sell)
 
-        # Adjust column weights
+
+    def disable_buttons(self):
+        self.sell_button.config(state="disabled")
+        self.delete_sell_button.config(state="disabled")
+        self.bid_button_bidding.config(state="disabled")
+
+    def set_timer(self):
+        timer_value = simpledialog.askinteger("Set Timer", "Enter the timer value in seconds:")
+        if timer_value is not None:
+            self.timer_seconds = timer_value
+            self.update_timer_display()
+            self.send_timer_info_to_server()  # Fixed: Call the method to send timer info to the server
+            self.start_timer()
+
+    def handle_echo_messages(self, topic, payload):
+        action = payload.get('Action', '')
+
+        if action == 'SetTimer':
+            timer_value = payload.get('TimerValue', 0)
+            seller_name = payload.get('Seller', 'Unknown Seller')
+            print(f"Received Timer Set Request from {seller_name}. Timer Value: {timer_value} seconds")
+
+    def enable_buttons(self):
+        self.sell_button.config(state="normal")
+        self.delete_sell_button.config(state="normal")
+        self.bid_button_bidding.config(state="normal")
 
     def place_sell(self):
         sell_item = simpledialog.askstring("Sell Item", "Enter item to sell:")
@@ -138,6 +164,7 @@ class TkEchoClient(BanyanBase):
                     self.publish_payload(payload, 'echo')
                 except Exception as e:
                     print(f"Error sending payload: {e}")
+                self.start_timer()
 
     def delete_sell_item(self):
         selected_index = self.items_selling.curselection()
@@ -176,7 +203,6 @@ class TkEchoClient(BanyanBase):
                 else:
                     print("Your bid is not higher than the current highest bid.")
 
-    # Add the following method to your TkEchoClient class
     def get_highest_bid(self, item):
         return self.highest_bids.get(item, 0.0)
 
@@ -210,24 +236,43 @@ class TkEchoClient(BanyanBase):
 
         Button(popup, text="Close", command=popup.destroy).pack(pady=10)
 
+    def send_timer_info_to_server(self):
+        payload = {'Action': 'SetTimer', 'TimerValue': self.timer_seconds, 'Seller': self.user_name}
+        self.publish_payload(payload, 'your_server_topic')  # Replace 'your_server_topic' with the actual server topic
+
+    def update_timer(self):
+        if self.timer_active and self.timer_seconds > 0:
+            self.timer_seconds -= 1
+            self.update_timer_display()
+            self.root.after(1000, self.update_timer)
+        elif self.timer_seconds == 0:
+            self.timer_active = False
+            self.disable_buttons()  # Disable the buttons when the timer reaches 0
+            # Add any additional logic you want to execute when the timer reaches 0
+
+
+
+    def update_timer_display(self):
+        formatted_time = time.strftime("%M:%S", time.gmtime(self.timer_seconds))
+        self.countdown_label.config(text=f"Time Left: {formatted_time} seconds")
+
+    def start_timer(self):
+        if self.timer_seconds > 0:
+            self.timer_active = True
+            self.enable_buttons()  # Enable the buttons when the timer starts
+            self.update_timer()  # Start the countdown immediately
 
 
     def get_message(self):
-        """
-        This method is called from the tkevent loop "after" call. It will poll for new zeromq messages
-        :return:
-        """
         try:
             data = self.subscriber.recv_multipart(zmq.NOBLOCK)
             self.incoming_message_processing(data[0].decode(), msgpack.unpackb(data[1]))
-            time.sleep(.001)
+            time.sleep(0.001)
             self.root.after(1, self.get_message)
-
         except zmq.error.Again:
             try:
-                time.sleep(.001)
+                time.sleep(0.001)
                 self.root.after(1, self.get_message)
-
             except KeyboardInterrupt:
                 self.root.destroy()
                 self.publisher.close()
@@ -242,20 +287,10 @@ class TkEchoClient(BanyanBase):
             sys.exit(0)
 
 
-    def on_closing(self):
-        """
-        Destroy the window
-        :return:
-        """
-        self.clean_up()
-        self.root.destroy()
-
-
 def gui_client():
     client = TkEchoClient()
-    # Add the main loop
+    client.get_message()
     client.root.mainloop()
-
 
 if __name__ == '__main__':
     gui_client()
